326251d5f58c846639fec79cc04a845d5b1d4db4
Abijeet123 <abijeetshyamm@gmail.com>
2022-02-27 19:07:01 +0000

    Created virtual machine

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
-clo: value.h value.c debug.c debug.h chunk.c chunk.h memory.c main.c
-	gcc value.c debug.c chunk.c memory.c main.c  -o cloxx
+clo: vm.c value.h value.c debug.c debug.h chunk.c chunk.h memory.c main.c
+	gcc common.h vm.c value.c debug.c chunk.c memory.c main.c  -o clox
+
+
 
--- main.c
+++ main.c
@@ -1,16 +1,26 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char **argv) {
+	initVM();
+
 	Chunk chunk;
 	initChunk(&chunk);
 	int constant = addConstant(&chunk, 1.2);
 	writeChunk(&chunk, OP_CONSTANT);
 	writeChunk(&chunk, constant);
+	constant = addConstant(&chunk, 1.3);
+	writeChunk(&chunk, OP_CONSTANT);
+	writeChunk(&chunk, constant);
+	writeChunk(&chunk, OP_DIVIDE);
 	writeChunk(&chunk, OP_RETURN);
 	
-	disassembleChunk(&chunk, "test chunk");
+	
+	//disassembleChunk(&chunk, "test chunk");
+	interpret(&chunk);
+	freeVM();
 	freeChunk(&chunk);
 	return 0;
 }
